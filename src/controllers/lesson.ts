a26bc0500cc7f4db83ba3be7136b97e4b913dace
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-06-18 23:09:39 +0500

    добавил обработку ошибок

--- src/controllers/lesson.ts
+++ src/controllers/lesson.ts
@@ -6,25 +6,69 @@ import { Request, Response } from "express";
 
 
 export const getLessons = async (req: Request, res: Response): Promise<void> => {
-    const { date = '', status = '', teacherIds = '', studentsCount = '', page = 1, lessonsPerPage = 5 } = req.query
+    try {
+        const { date = '', status = '', teacherIds = '', studentsCount = '', page = 1, lessonsPerPage = 5 } = req.query
 
-
-
-    const lessons = await getLessonsService(String(date), String(status), String(teacherIds), String(studentsCount), +page || 1, +lessonsPerPage || 5)
-
-    res.json({
-        lessons
-    })
+        const lessons = await getLessonsService(String(date), String(status), String(teacherIds), String(studentsCount), +page || 1, +lessonsPerPage || 5)
+    
+        res.json({
+            success: true,
+            lessons
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(400).json({
+                success: false,
+                message: error.message ?? error
+            })
+        } else {
+            res.json({
+                success: false,
+                message: "Непредвиденная ошибка"
+            })
+        }
+    }
 }
 
 export const createLessons = async (req: Request, res: Response): Promise<void> => {
 
-    const {teacherIds, title, days, firstDate, lessonsCount, lastDate} = req.body
+    try {
+        const {teacherIds, title, days, firstDate, lessonsCount, lastDate} = req.body
 
-    const lessons = await createLessonsService(teacherIds, title, days, firstDate, lessonsCount, lastDate)
+        if (!Array.isArray(teacherIds)) {
+            throw new Error('teachersId должно передаваться в формате [1, 2]')
+        }
 
-    res.json({
-        lessons
-    })
+        if (!Array.isArray(days)) {
+            throw new Error('days должно передаваться в формате [1, 2], если нужно добавить урок только на один день, передайте номер дня в массиве с одним элементом, например [4]')
+        }
+
+        if (lessonsCount !== undefined && typeof +lessonsCount !== 'number') {
+            throw new Error('lessonsCount должно быть числом или строкой которая преобразуется в число')
+        }
+
+        if (!title) {
+            throw new Error('поле "title" обязательно, пример "Pink Color"')
+        }
+    
+        const lessons = await createLessonsService(teacherIds, title, days, firstDate, +lessonsCount, lastDate)
+    
+        res.json({
+            success: true,
+            lessons
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(400).json({
+                success: false,
+                message: error.message ?? error
+            })
+        } else {
+            res.json({
+                success: false,
+                message: "Непредвиденная ошибка"
+            })
+        }
+    }
 
 }
--- src/services/lesson.ts
+++ src/services/lesson.ts
@@ -24,79 +24,86 @@ export const getLessonsService = async (date: string, status: string, teacherIds
 
 export const createLessonsService = async (teacherIds: number[], title: string, days: number[], firstDate: string, lessonsCount: number | undefined, lastDate: string | undefined): Promise<number[]> => {
     
-    const lessonsForInsert: Pick<Lesson, 'date' | 'title' | 'status'>[] = []
-    
+    try {
+        const lessonsForInsert: Pick<Lesson, 'date' | 'title' | 'status'>[] = []
+        
+        // форматируем даты
 
-    // форматируем даты
-
-    const firstDateFormat = moment(new Date(firstDate)) 
-    const lastDateFormat = lastDate && moment(new Date(lastDate)) || null
-    let currentDate =  moment(new Date(firstDate))
+        const firstDateFormat = moment(new Date(firstDate)) 
+        const lastDateFormat = lastDate && moment(new Date(lastDate)) || null
 
 
-    let addedCount = 0
-    let currentCount = 0
-
-    
-
-    if (lastDate || lessonsCount) { // если задали каунт или последнюю дату значит урок очевидно не один
-        while (
-            (
-                (lessonsCount && lessonsCount > addedCount)  // если есть каунт
-                    || 
-                (lastDateFormat && lastDateFormat.diff(currentDate, 'days') > 0)  // если есть дата
-            )
-            && currentDate.diff(firstDateFormat, 'years') <= 0 // не даем дальше чем на год вперед залезать
-            && currentCount < 300 // и не более 300 записей
-            ) {
-    
-            const dayOfWeek = currentDate.day() // узнаем индекс дня недели
-    
-    
-    
-            if (days.includes(dayOfWeek)) { // записываем если день недели подходит 
-                lessonsForInsert.push({
-                    date: currentDate.toDate(),
-                    title: title,
-                    status: 0
-                })
-                addedCount++ // инкремент каунта добавления
-            }
-    
-            currentDate.add(1, 'days') // инкремент дня
-            currentCount++ // инкремент каунта итерации
+        if (firstDateFormat.toString() === 'Invalid date' || (lastDateFormat && lastDateFormat.toString() === 'Invalid date')) {
+            throw new Error('Дата задана не в правильном формате, правильный формат: "2023-07-23"')
         }
-    } else { // если не задали значит добавляем один урок
-        lessonsForInsert.push({
-            date: currentDate.toDate(),
-            title: title,
-            status: 0
-        })
-    }
+
+        let currentDate =  moment(new Date(firstDate))
 
 
+        let addedCount = 0
+        let currentCount = 0
 
+        
 
-    const lessons = await Lesson // инсертим уроки
-        .query()
-        .insert(lessonsForInsert)
+        if (lastDate || lessonsCount) { // если задали каунт или последнюю дату значит урок очевидно не один
+            while (
+                (
+                    (lessonsCount && lessonsCount > addedCount)  // если есть каунт
+                        || 
+                    (lastDateFormat && lastDateFormat.diff(currentDate, 'days') > 0)  // если есть дата
+                )
+                && currentDate.diff(firstDateFormat, 'years') <= 0 // не даем дальше чем на год вперед залезать
+                && currentCount < 300 // и не более 300 записей
+                ) {
+        
+                const dayOfWeek = currentDate.day() // узнаем индекс дня недели
+        
+        
+        
+                if (days.includes(dayOfWeek)) { // записываем если день недели подходит 
+                    lessonsForInsert.push({
+                        date: currentDate.toDate(),
+                        title: title,
+                        status: 0
+                    })
+                    addedCount++ // инкремент каунта добавления
+                }
+        
+                currentDate.add(1, 'days') // инкремент дня
+                currentCount++ // инкремент каунта итерации
+            }
+        } else { // если не задали значит добавляем один урок
+            lessonsForInsert.push({
+                date: currentDate.toDate(),
+                title: title,
+                status: 0
+            })
+        }
 
-
-    const lessonsIds = lessons.map(lesson => lesson.id) // создаем массив айдишников
-
-    for (const teacherId of teacherIds) { // итерируем по учителям
-
-        const lessonTeachersForInsert = lessonsIds.map(lessonId => ({ // создаем объекты для пуша в таблицу учителя-уроки
-            lesson_id: +lessonId,
-            teacher_id: +teacherId
-        })) 
-
-        await LessonTeacher // инсертим учителя уроки
+        const lessons = await Lesson // инсертим уроки
             .query()
-            .insert(lessonTeachersForInsert)
-            .returning('lesson_id')
-    }
+            .insert(lessonsForInsert)
 
-    return lessonsIds
+
+        const lessonsIds = lessons.map(lesson => lesson.id) // создаем массив айдишников
+
+        for (const teacherId of teacherIds) { // итерируем по учителям
+
+            const lessonTeachersForInsert = lessonsIds.map(lessonId => ({ // создаем объекты для пуша в таблицу учителя-уроки
+                lesson_id: +lessonId,
+                teacher_id: +teacherId
+            })) 
+
+            await LessonTeacher // инсертим учителя уроки
+                .query()
+                .insert(lessonTeachersForInsert)
+                .returning('lesson_id')
+        }
+
+        return lessonsIds
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 
 }
